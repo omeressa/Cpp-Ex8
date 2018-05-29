cf5ca08bd8c58b1abddaf90073f92443c311baa7
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-05-29 17:00:35 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -93,94 +93,125 @@ ostream& operator << (ostream & os, const Board & board){
 
 ////////////////////////////////////////////////////////////
 
-istream& operator>> (istream& is, Board& b){
-    string filename,Name;
-    is >> filename;
-    ifstream infile;
-    infile.open(filename);
-    
-    /* file not fount exiption */
+    istream& operator>> (istream &is,  Board &board){
+        string file;
+        is>>file;
+        int size=file.length();
+        Board temp(size);
+        int i;
+        for(i=0; i<size; i++) temp[{0,i}] = file[i];
+        int num = 1;
+        while(is>>file){
+            i=0;
+            for(i=0; i<size; i++) temp[{num, i}] = file[i];
+            num++;
+        }
+        b=temp;
+        return is;
+    }
 
-    if(infile.fail()){
-      cout << "file not found !!!" << endl;
-      exit(1);
-    }
-    
-    uint i=0;
-    infile >> Name;
-    Board temp((int)Name.Size());
-    while (i<temp.Size()) {
-      if(i!=0)
-        infile >> Name;
-      for (uint j = 0; j < temp.Size(); j++) {
-        temp[{i,j}] = Name[j];
-      }
-      i++;
-    }
-    b = temp;
-    infile.close();
-    return is;
-}
+
+  string Board::draw(int num){
+     Board board{size};
+     board=*this;
+	string fileName ="TicTacToe.ppm";
+	int length = num, width = num;
+	ofstream output(fileName, ios::app |ios::binary );
+	output << "P6" << endl << length <<" " << width << endl << 255 << endl;
+
+	RedGreenBlue image[length*length];
+        
+    for(int j=0; j<length;j++){  // row
+       for(int i=0; i<length;i++){ // column
+/*
+*giving all three the number 255 gives us a white board pic
+*/
+           image[(length*j)+i].red =255;
+           image[(length*j)+i].green = 255;
+           image[(length*j)+i].blue =255; 
+         }
+       }
 
 /*
-istream &operator>>(istream &in,  Board &b)
-{
-    Board temp;
-    in >> temp;
-    b = temp ;
-    return in;
-}*/
+* creat lines
+*/
+	for(int i=0;i<size;++i){//create col
+		int len =i*(length/size);
+		for(int j=10;j<length-10;++j){
+/*
+*giving only the blue one 255 and others 
+* give them zero makes our TicTacToe board col a blue col
+*/
+			image[(length*j)+len].red =0;
+			image[(length*j)+len].green =0;
+			image[(length*j)+len].blue =255;
+		}
+	}
+	for(int i=0;i<size;++i){//create rows
+	    int wid=i*(length/size);
+        for(int j=10;j<length-10;++j){
+/*
+*giving only the blue one 255 and others 
+* give them zero makes our TicTacToe board rows a blue rows
+*/
+          	image[(wid*length)+j].red =0;
+          	image[(wid*length)+j].green =0;
+            image[(wid*length)+j].blue =255;
+		}
+	}
+	for(int i=0;i<size;++i){//X and Y signs
+		for(int j=0;j<size;++j){
+            int len, to_len, wid, to_wid;
+			len =j*(length/size);
+		    to_len =(j+1)*(length/size);
+			wid =i*(length/size);
+			to_wid =(i+1)*(length/size);
+            
+            if(board[{i,j}] =='O'){//draw O
+				   int len_dist =(to_len-len)/2;
+                   int wid_dist =(to_wid-wid)/2;
+                   int rad = len_dist;
+				   for(int i=0;i<to_wid-wid-10;++i){
+					  int j=sqrt((rad*rad)-(i-len_dist)*(i-len_dist))+wid_dist;
+					  int a =5;
+/*
+*sign O will be black cuz all green red and blue = 0;
+*/
+                      image[length*((wid+j)-a)+len+i].green =0;
+          			  image[length*((wid+j)-a)+len+i].blue =0;
+                      image[length*((wid+j)-a)+len+i].red =0;
+          			  image[length*(to_wid+a-j)+len+i].green =0;
+          			  image[length*(to_wid+a-j)+len+i].blue =0;
+                      image[length*(to_wid+a-j)+len+i].red =0;
+				}
+			}
+/*
+*giving all green red and blue zero ,makes our X sign color black
+*/
+		else if(board[{i,j}] =='X'){ // draw X
+			   for(int t=0;t<to_wid-wid-10;++t) {
+                   image[length*(t+wid)+len+t].green =0;
+                   image[length*(t+wid)+len+t].red =0;
+	               image[length*(t+wid)+len+t].blue =0;
+		           image[length*(t+wid)+to_len-t].blue =0;
+                   image[length*(t+wid)+to_len-t].green =0;
+                   image[length*(t+wid)+to_len-t].red =0;
+				}
+			} 
+		}
+	}
 
+/*
+*image processing
+*/
+	output.write (reinterpret_cast <char*>(& image),3 * num*num);
+	output.close ();
+	return fileName;
+}
 
-string Board::draw(int n){
-
-      const int length = n, width = n;
-      string fileName = "TicTacToe.ppm";
-      ifstream File(fileName);
-        bool fileExist = false;
-
-        if(File){
-            cout << "does exist!!!" << endl;
-            fileExist = true;
-        }
-
-        int index = 1;
-
-        while(fileExist){
-            fileName = "TicTacToe";
-            fileName += to_string(index);
-            fileName += ".ppm";
-
-            ifstream File(fileName);
-            if(File) {index++;}
-            else {fileExist=false;}
-        }
-
-    ofstream imageFile(fileName);//create new file
-      cout << "omar_board: " << fileName << endl;
-
-      imageFile << "P6" << endl << length <<" " << width << endl << 255 << endl;
-      RedGreenBlue image[length*length];
-      for (int j = 0; j < length; ++j){  
-        for (int i = 0; i < length; ++i){
-           image[(length*j)+i].red = (i % 256);
-           image[(length*j)+i].green = (j % 256);
-           image[(length*j)+i].blue = ( ((i*i)+(j*j)) % 256);
-         }
-      }
     /*
     *    http://www.informit.com/articles/article.aspx?p=328647
     */
-      image[0].red = 255;
-      image[0].blue = 0;
-      image[0].green = 0;
-    
-      imageFile.write(reinterpret_cast<char*>(&image), 3*length*length); // processing image
-      imageFile.close();
-      return "TicTacToe.ppm";
-    }
-
-
 
 
 
