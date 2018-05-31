f513014ae770b3cd0dc6448eabb280628a45ccc2
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-05-31 14:51:10 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -243,7 +243,10 @@ istream& operator>> (istream &is,  Board &board){
 *
 *
 */
-	  if(num ==2){
+    if((num==1)||(num ==2)||(num ==3)||(num ==4)||(num ==5)||(num ==6)
+       ||(num ==7)||(num ==8)||(num ==9)||(num ==10)||(num ==11)||(num ==12)
+       ||(num ==13)||(num ==14)||(num ==15)||(num ==16)||(num ==17)||(num ==18)
+       ||(num ==19)||(num ==20)){
 		  
 	int length = num, width = num;
 	string fileName = "TicTacToe_"+to_string(size)+".ppm";// this way we can make all type of dif board with dif image names
